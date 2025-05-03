b992843ddd335f67a9130d3032a955c00424942b
Poornima715 <2023cs_poornimadevangamath_c@nie.ac.in>
2025-05-03 15:37:56 +0530

    Add migrate script for Render deployment

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HomeServices.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homeservices.settings')
 
     try:
         from django.core.management import execute_from_command_line
