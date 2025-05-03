181c4d23cda101c505a61047e6a3cb59a36595e2
Poornima715 <2023cs_poornimadevangamath_c@nie.ac.in>
2025-05-03 16:20:32 +0530

    Add temporary URLs to run migrate and collectstatic

--- OnDemand_HomeServices/urls.py
+++ OnDemand_HomeServices/urls.py
@@ -6,3 +6,19 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
+from django.urls import path
+from django.http import HttpResponse
+from django.core.management import call_command
+
+def trigger_migrate(request):
+    call_command('migrate')
+    return HttpResponse("Migration done.")
+
+def trigger_collectstatic(request):
+    call_command('collectstatic', interactive=False)
+    return HttpResponse("Collectstatic done.")
+
+urlpatterns += [
+    path('run-migrate/', trigger_migrate),
+    path('run-collectstatic/', trigger_collectstatic),
+]
